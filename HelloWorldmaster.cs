ccdb1834b17a631bfc289ac17d2aebaf6fcadfe7
Jacob Polden <jacob.polden@valtech.co.uk>
2018-12-05 16:17:34 +0000

    hello world

--- HelloWorldmaster.cs
+++ HelloWorldmaster.cs
@@ -13,21 +13,15 @@ namespace Company.Function
     public static class HelloWorld
     {
         [FunctionName("HelloWorld")]
-        public static async Task<IActionResult> Run(
+        public static IActionResult Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
+            var name = Environment.GetEnvironmentVariable("HELLO_WORLD_NAME") ?? "World";
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+            return (ActionResult)new OkObjectResult($"Hello {name}");
         }
     }
 }
